f00026b66e048364356c97e5d0960ae7f6895fbe
권혁 <kwon8999@supermvoe.co.kr>
2022-11-22 17:42:19 +0900

    [권 혁] 테마 관런 작업

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,17 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Create by kwonhyeok on 2022/11/21
+ * **/
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
+
